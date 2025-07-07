ad8c81821132d516848f4a2cc3c33d246c4df780
JosanoPereira <josanojp@gmail.com>
2025-07-07 09:20:12 +0100

    fix: resolvendo erro no middleware

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,13 +47,13 @@ return [
         'public' => env('APP_ENV') === 'production'
             ? [
                 'driver' => 's3',
-                'key' => env('LARAVEL_CLOUD_PUBLIC_ACCESS_KEY_ID'),
-                'secret' => env('LARAVEL_CLOUD_PUBLIC_ACCESS_KEY_SECRET'),
-                'region' => env('LARAVEL_CLOUD_PUBLIC_DEFAULT_REGION', 'auto'),
-                'bucket' => env('LARAVEL_CLOUD_PUBLIC_BUCKET'),
-                'url' => env('LARAVEL_CLOUD_PUBLIC_URL'),
-                'endpoint' => env('LARAVEL_CLOUD_PUBLIC_ENDPOINT'),
-                'use_path_style_endpoint' => env('LARAVEL_CLOUD_PUBLIC_USE_PATH_STYLE_ENDPOINT', false),
+                'key' => env('AWS_ACCESS_KEY_ID'),
+                'secret' => env('AWS_SECRET_ACCESS_KEY'),
+                'region' => env('AWS_DEFAULT_REGION', 'auto'),
+                'bucket' => env('AWS_BUCKET'),
+                'url' => env('AWS_URL'),
+                'endpoint' => env('AWS_ENDPOINT'),
+                'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
                 'visibility' => 'public',
             ]
             : [
